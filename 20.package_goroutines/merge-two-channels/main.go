051d4fd6ac547e6d78f7c168fde94fd2e6793f07
Dennis Mattews <alttert@mail.ru>
2025-01-19 22:48:07 +0300

    merge two chans with explaination

--- 20.package_goroutines/merge-two-channels/main.go
+++ 20.package_goroutines/merge-two-channels/main.go
@@ -3,38 +3,44 @@ package main
 import "fmt"
 
 func main() {
-	ch1 := make(chan int)
-	ch2 := make(chan int)
-	mergedCh := make(chan int)
+	// Создаем три канала для передачи целых чисел
+	oniChan1 := make(chan int)    // Канал для первой горутины
+	oniChan2 := make(chan int)    // Канал для второй горутины
+	mergedChans := make(chan int) // Канал для объединения значений из двух предыдущих каналов
 
+	// Запускаем первую горутину, которая заполняет oniChan1 числами от 1 до 5
 	go func() {
-		defer close(ch1)
+		defer close(oniChan1) // Закрываем канал oniChan1 после завершения работы горутины
 		for i := 1; i <= 5; i++ {
-			ch1 <- i
+			oniChan1 <- i // Отправляем числа от 1 до 5 в канал oniChan1
 		}
 	}()
 
+	// Запускаем вторую горутину, которая заполняет oniChan2 числами от 6 до 10
 	go func() {
-		defer close(ch2)
+		defer close(oniChan2) // Закрываем канал oniChan2 после завершения работы горутины
 		for i := 6; i <= 10; i++ {
-			ch2 <- i
+			oniChan2 <- i // Отправляем числа от 6 до 10 в канал oniChan2
 		}
 	}()
 
+	// Запускаем третью горутину, которая читает из oniChan1 и отправляет значения в mergedChans
 	go func() {
-		defer close(mergedCh)
-		for num := range ch1 {
-			mergedCh <- num
+		defer close(mergedChans)    // Закрываем mergedChans после завершения работы этой горутины
+		for num := range oniChan1 { // Читаем значения из oniChan1 до его закрытия
+			mergedChans <- num // Отправляем прочитанные значения в mergedChans
 		}
 	}()
 
+	// Запускаем четвертую горутину, которая читает из oniChan2 и отправляет значения в mergedChans
 	go func() {
-		for num := range ch2 {
-			mergedCh <- num
+		for num := range oniChan2 { // Читаем значения из oniChan2 до его закрытия
+			mergedChans <- num // Отправляем прочитанные значения в mergedChans
 		}
 	}()
 
-	for num := range mergedCh {
-		fmt.Println(num)
+	// В основном потоке читаем значения из mergedChans и выводим их на экран
+	for num := range mergedChans {
+		fmt.Println(num) // Печатаем каждое число, полученное из mergedChans
 	}
 }

8e98e3179a0a7688d84c2d944862d0939876fb9c
Dennis Mattews <alttert@mail.ru>
2025-01-19 23:22:24 +0300

    bobuilder updated with comments

--- 3.package_strings/bobuilder/main.go
+++ 3.package_strings/bobuilder/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	fmt.Println("BOBUILDER")
+	var bobuilder strings.Builder // Создаём Builder для эффективного построения строк.
+
+	// Добавляем строки по очереди.
+	bobuilder.WriteString("Go")                      // Добавляем "Go".
+	bobuilder.WriteString(" ")                       // Добавляем пробел.
+	bobuilder.WriteString("is awesome!")             // Добавляем "is awesome!".
+	fmt.Println("Built string:", bobuilder.String()) // Выводим: Built string: Go is awesome!
+
+	// Получаем текущую длину строки в Builder.
+	fmt.Println("Length:", bobuilder.Len()) // Длина: 13.
+
+	// Очищаем содержимое Builder.
+	bobuilder.Reset()
+	fmt.Println("After reset:", bobuilder.String()) // После очистки: пустая строка.
+
+	// Добавляем форматированную строку.
+	fmt.Fprintf(&bobuilder, "Formatted number: %d", 42)
+	fmt.Println("Formatted string:", bobuilder.String()) // Выводим: Formatted string: Formatted number: 42.
 }

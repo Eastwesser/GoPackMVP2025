a14344ea9cb49343a06adbd336e399e1f69ff53b
Dennis Mattews <alttert@mail.ru>
2025-01-19 20:41:17 +0300

    goroutines for int & string

--- 20.package_goroutines/main.go
+++ 20.package_goroutines/main.go
@@ -2,18 +2,21 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	oniChan := make(chan int)
+	oniChan := make(chan string)
+	oniChan2 := make(chan int)
 
-	go say("Hello World", oniChan)
-	time.Sleep(2 * time.Second)
-	fmt.Println(<-oniChan)
+	go say("Hello World", oniChan, oniChan2)
+	//time.Sleep(2 * time.Second)
+	data1 := <-oniChan
+	data2 := <-oniChan2
+	fmt.Println(data1, data2)
 }
 
-func say(greetings string, oniChan chan int) {
+func say(greetings string, oniChan chan string, oniChan2 chan int) {
 	fmt.Println(greetings)
-	oniChan <- 7
+	oniChan <- "HI HI"
+	oniChan2 <- 1000
 }

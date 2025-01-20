c915736956b9778671fd50ceb9db2495b0e08c0b
Dennis Mattews <alttert@mail.ru>
2025-01-20 04:00:11 +0300

    added simple pointer case

--- 4.package_pointer/pointer-mointer/main.go
+++ 4.package_pointer/pointer-mointer/main.go
@@ -1,1 +1,19 @@
-package pointer_mointer
+package main
+
+import "fmt"
+
+func pointerMointer(afloat *float64) *float64 {
+	*afloat += 1
+
+	return afloat
+}
+
+func main() {
+	var a float64
+	fmt.Println("Введите ваше число в консоль: ")
+	fmt.Scanln(&a)
+	fmt.Println(*pointerMointer(&a))
+}
+
+// Напиши функцию которая принимает указатель на float, увеличивает значение числа на 1,
+// с помощью инкремента и возвращает указатель на указатель на это число. Вызови эту функцию, в Print разыменуй указатель

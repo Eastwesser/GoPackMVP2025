e65288db42bafb49daf60e337715008fb52f01d2
Dennis Mattews <alttert@mail.ru>
2025-01-25 04:00:30 +0300

    sudoku validation success

--- 7.package_sudoku-maps/sudoku-validation/main.go
+++ 7.package_sudoku-maps/sudoku-validation/main.go
@@ -2,29 +2,42 @@ package main
 
 import "fmt"
 
+/*
+board.length == 9
+board[i].length == 9
+board[i][j] is a digit 1-9 or '.'.
+*/
+
 func isValidSudoku(board [][]byte) bool {
-	sudoku := make([][]int, len(board))
-	desicion := false
+	desicion := true // if there are no dupes - true is as default
 
-	if len(sudoku) != len(board[0]) {
-		desicion = false
-	} else if len(sudoku) != len(board) {
-		desicion = false
-	} else {
-		for i := 0; i < len(sudoku); i++ {
-			for j := 0; j < len(sudoku[i]); j++ {
-				return true
+	cols := make([]map[byte]bool, 9)    // idx_col
+	rows := make([]map[byte]bool, 9)    // idx_row
+	squares := make([]map[byte]bool, 9) // idx_row, idx_col
+
+	for i := 0; i < 9; i++ {
+		cols[i] = make(map[byte]bool)    // init map for columns
+		rows[i] = make(map[byte]bool)    // init map for rows
+		squares[i] = make(map[byte]bool) // init map for sudoku-boxes
+	}
+
+	for i := 0; i < len(board); i++ {
+		for j := 0; j < len(board[0]); j++ {
+			num := board[i][j]
+
+			if num == '.' {
+				continue
 			}
-		}
-	}
 
-	for i := 0; i < len(sudoku); i++ {
-		sudoku[i] = make([]int, len(board[0]))
-	}
+			boxIndex := (i/3)*3 + j/3
 
-	for i := 0; i < len(sudoku); i++ {
-		for j := 0; j < len(sudoku[0]); j++ {
-			return true
+			if rows[i][num] || cols[j][num] || squares[boxIndex][num] {
+				desicion = false
+			}
+
+			cols[j][num] = true
+			rows[i][num] = true
+			squares[boxIndex][num] = true
 		}
 	}
 
@@ -32,35 +45,30 @@ func isValidSudoku(board [][]byte) bool {
 }
 
 func main() {
-
-	boardTrue := {
-			{"5", "3", ".", ".", "7", ".", ".", ".", "."},
-			{"6", ".", ".", "1", "9", "5", ".", ".", "."},
-			{".", "9", "8", ".", ".", ".", ".", "6", "."},
-			{"8", ".", ".", ".", "6", ".", ".", ".", "3"},
-			{"4", ".", ".", "8", ".", "3", ".", ".", "1"},
-			{"7", ".", ".", ".", "2", ".", ".", ".", "6"},
-			{".", "6", ".", ".", ".", ".", "2", "8", "."},
-			{".", ".", ".", "4", "1", "9", ".", ".", "5"},
-			{".", ".", ".", ".", "8", ".", ".", "7", "9"},
+	boardTrue := [][]byte{
+		{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+		{'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+		{'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+		{'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+		{'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+		{'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+		{'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+		{'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+		{'.', '.', '.', '.', '8', '.', '.', '7', '9'},
 	}
 
-
-	boardFalse := {
-		{"5", "3", ".", ".", "7", ".", ".", ".", "."},
-		{"6", ".", ".", "1", "9", "5", ".", ".", "."},
-		{".", "9", "8", ".", ".", ".", ".", "6", "."},
-		{"8", ".", ".", ".", "6", ".", ".", ".", "3"},
-		{"4", ".", ".", "8", ".", "3", ".", ".", "1"},
-		{"7", ".", ".", ".", "2", ".", ".", ".", "6"},
-		{".", "6", ".", ".", ".", ".", "2", "8", "."},
-		{".", ".", ".", "4", "1", "9", ".", ".", "5"},
-		{".", ".", ".", ".", "8", ".", ".", "7", "9"},
+	boardFalse := [][]byte{
+		{'8', '3', '.', '.', '7', '.', '.', '.', '.'},
+		{'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+		{'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+		{'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+		{'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+		{'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+		{'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+		{'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+		{'.', '.', '.', '.', '8', '.', '.', '7', '9'},
 	}
 
-	rightCase := isValidSudoku(boardTrue)
-	wrongCase := isValidSudoku(boardFalse)
-
-	fmt.Println(rightCase)
-	fmt.Println(wrongCase)
+	fmt.Println("Valid Board:", isValidSudoku(boardTrue))    // true
+	fmt.Println("Invalid Board:", isValidSudoku(boardFalse)) // false
 }

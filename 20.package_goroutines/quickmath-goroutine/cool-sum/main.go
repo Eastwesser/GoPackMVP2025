7c8799468409bfd4b3937243807792c6979497f3
Dennis Mattews <alttert@mail.ru>
2025-02-02 04:45:28 +0300

    cool sum example

---
+++ 20.package_goroutines/quickmath-goroutine/cool-sum/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func cringeGoroutine(firstNum, secondNum int) chan int {
+	oniChan := make(chan int)
+
+	go func() {
+		defer close(oniChan)
+
+		c := firstNum + secondNum
+		oniChan <- c
+	}()
+
+	return oniChan
+}
+
+func main() {
+	var a, b int
+
+	fmt.Scan(&a, &b)
+	result := cringeGoroutine(a, b)
+
+	fmt.Println(<-result) // если не поставить '<-' (чтение канала) мы получим адрес памяти канала
+}

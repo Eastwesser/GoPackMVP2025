9e2783553eecbc92e20d2a8ecbce96f7ef5c084e
Dennis Mattews <alttert@mail.ru>
2025-02-02 04:54:26 +0300

    cringe goroutine upd

--- 20.package_goroutines/anomaly-goroutines/main.go
+++ 20.package_goroutines/anomaly-goroutines/main.go
@@ -1,25 +1,70 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
 
-func cringeGoroutine(firstNum, secondNum int) chan int {
-	oniChan := make(chan int)
+// Кринжовая горутина
+func cringeGoroutine(ctx context.Context, wg *sync.WaitGroup, dataChan chan<- int) {
+	defer wg.Done() // Уменьшаем счетчик WaitGroup при завершении
 
-	go func() {
-		defer close(oniChan)
+	for {
+		select {
+		case <-ctx.Done(): // Если пришел сигнал завершения
+			fmt.Println("Горутина: меня остановили, кринж...")
+			return
+		default:
+			// Спим случайное время (от 100 до 500 мс)
+			sleepTime := time.Duration(rand.Intn(400)+100) * time.Millisecond
+			fmt.Printf("Горутина: сплю %v...\n", sleepTime)
+			time.Sleep(sleepTime)
 
-		c := firstNum + secondNum
-		oniChan <- c
-	}()
+			// Генерируем случайное число
+			num := rand.Intn(100)
+			fmt.Printf("Горутина: сгенерировала число %d\n", num)
 
-	return oniChan
+			// Если число делится на 3, отправляем его в канал
+			if num%3 == 0 {
+				fmt.Printf("Горутина: число %d делится на 3, отправляю в канал\n", num)
+				dataChan <- num
+			} else {
+				fmt.Printf("Горутина: число %d не делится на 3, пропускаю\n", num)
+			}
+		}
+	}
 }
 
 func main() {
-	var a, b int
+	// Создаем контекст с таймаутом для завершения горутины через 5 секунд
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
-	fmt.Scan(&a, &b)
-	result := cringeGoroutine(a, b)
+	// Канал для передачи данных
+	dataChan := make(chan int)
 
-	fmt.Println(<-result) // если не поставить '<-' (чтение канала) мы получим адрес памяти канала
+	// WaitGroup для ожидания завершения горутины
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	// Запускаем кринжовую горутину
+	go cringeGoroutine(ctx, &wg, dataChan)
+
+	// Горутина для чтения данных из канала
+	go func() {
+		for num := range dataChan {
+			fmt.Printf("Главная функция: получила число %d из канала\n", num)
+		}
+	}()
+
+	// Ждем завершения горутины
+	wg.Wait()
+
+	// Закрываем канал после завершения
+	close(dataChan)
+
+	fmt.Println("Программа завершена. Кринж закончился.")
 }

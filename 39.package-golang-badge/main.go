1ecc4dc138c6ebec56faa85cc47c3243891d24b4
Dennis Mattews <alttert@mail.ru>
2025-03-29 02:00:52 +0300

    badge code

---
+++ 39.package-golang-badge/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"runtime"
+	"time"
+)
+
+var start = time.Now()
+
+func EastwesserBadge(w http.ResponseWriter, _ *http.Request) {
+	fmt.Fprintf(w, "🚀 Eastwesser | Go %s | Uptime: %s",
+		runtime.Version(),
+		time.Since(start).Round(time.Second))
+}
+
+func main() {
+	http.HandleFunc("/", EastwesserBadge)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		return
+	}
+}

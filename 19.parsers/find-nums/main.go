d44471a560a7302be130790a5294b6179883512a
Денис Матвеев <alttert@mail.ru>
2025-04-25 23:36:21 +0300

    find all numbers from text and sum them

---
+++ 19.parsers/find-nums/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	content, err := ioutil.ReadFile("text.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	text := string(content)
+
+	// Regular expression to find numbers in the text
+	re := regexp.MustCompile(`(\d[\d\s]*\d|\d)`)
+
+	total := 0
+
+	// Process each line
+	lines := strings.Split(text, "\n")
+	for _, line := range lines {
+		matches := re.FindAllString(line, -1)
+		if len(matches) == 0 {
+			continue
+		}
+
+		// Process each number found in the line
+		for _, match := range matches {
+			cleanNum := strings.ReplaceAll(match, " ", "")
+			num, err := strconv.Atoi(cleanNum)
+			if err == nil {
+				total += num
+			}
+		}
+	}
+
+	fmt.Printf("Total sum of all numbers: %d\n", total)
+}

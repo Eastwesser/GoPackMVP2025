29b2c06167aa9eaf5827c5924d6119f25eaac60e
Денис Матвеев <alttert@mail.ru>
2025-04-25 23:39:17 +0300

    improved the code

--- 19.parsers/find-nums/main.go
+++ 19.parsers/find-nums/main.go
@@ -10,32 +10,17 @@ import (
 )
 
 func main() {
-	content, err := os.ReadFile("text.txt")
+	text, err := os.ReadFile("text.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-	text := string(content)
 
-	// Regular expression to find numbers in the text
-	re := regexp.MustCompile(`(\d[\d\s]*\d|\d)`)
-
+	re := regexp.MustCompile(`\d{1,3}(?:\s?\d{3})*`)
 	total := 0
 
-	// Process each line
-	lines := strings.Split(text, "\n")
-	for _, line := range lines {
-		matches := re.FindAllString(line, -1)
-		if len(matches) == 0 {
-			continue
-		}
-
-		// Process each number found in the line
-		for _, match := range matches {
-			cleanNum := strings.ReplaceAll(match, " ", "")
-			num, err := strconv.Atoi(cleanNum)
-			if err == nil {
-				total += num
-			}
+	for _, match := range re.FindAllString(string(text), -1) {
+		if num, err := strconv.Atoi(strings.ReplaceAll(match, " ", "")); err == nil {
+			total += num
 		}
 	}
 

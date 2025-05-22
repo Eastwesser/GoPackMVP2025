4a003d493da01ee4010f0321b90339bd765d65ed
Денис Матвеев <alttert@mail.ru>
2025-05-22 04:54:27 +0300

    revived all back, emp -> user

---
+++ interviews/mock-interview-4/third-test/andrius/http-restlike-api/main.go
@@ -0,0 +1,19 @@
+package http_restlike_api
+
+const a = "asd"
+git restore 0.go-core/5.package_regexp/basics/main.go
+git restore 0.go-core/8.package_faker/main.go
+git restore 2.goroutines-oop-patterns/22.package_mockery/1.simple-mock/main.go
+git restore 3.server-tasks/36.Dockerizer/simple-example/dc-examples/with-db/docker-compose.yml
+git restore 3.server-tasks/37.Qiqi/CRUD-chi/main.go
+git restore 4.clean-architecture-cruds/42.TAXI-DADATA/dadata-taxi-api/geo-api/cmd/main.go
+git restore 4.clean-architecture-cruds/44.Clean-CRUDs/main.go
+git restore 4.clean-architecture-cruds/45.Clean-Arch/README.md
+git restore 4.clean-architecture-cruds/45.Clean-Arch/internal/controllers/routers.go
+git restore 4.clean-architecture-cruds/45.Clean-Arch/internal/domain/models.go
+git restore 4.clean-architecture-cruds/45.Clean-Arch/internal/repository/repository.go
+git restore 4.clean-architecture-cruds/45.Clean-Arch/internal/usecase/usecase.go
+git restore 4.clean-architecture-cruds/46.REDIS/redis-example/main.go
+git restore 9.pet-projects/93.package-tg-bot/pompon-bot-golang/internal/handlers/order.go
+git restore interviews/mock-interview-4/first-half/task1-http-duplicator/clean/review/main.go
+git restore interviews/mock-interview-4/second-half/andrius/old/andrius-cleanarch-map/internal/interfaces/handler.go

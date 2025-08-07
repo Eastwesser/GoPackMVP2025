9219dbb1dd426eec9408a829c021bf8877978a62
Денис Матвеев <alttert@mail.ru>
2025-08-07 23:08:21 +0300

    palindrome bad choice

--- 6.leet-code/61.task_1_palindrome_string/main.go
+++ 6.leet-code/61.task_1_palindrome_string/main.go
@@ -2,28 +2,38 @@ package main
 
 import (
 	"fmt"
+	"regexp"
 	"strings"
 )
 
+// 125. Valid Palindrome (задача на словарный палиндром)
 func isPalindrome(s string) bool {
+	var straightOrder string
+	var reverseOrder string
+
+	// приводим буквы  в 's' к нижнему регистру
 	s = strings.ToLower(s)
-	i, j := 0, len(s)-1
-	for i < j {
-		if s[i] != s[j] {
-			return false
-		}
+
+	// удаляем в 's' все, кроме букв
+	reg := regexp.MustCompile(`[^a-z0-9]`)
+	clean := reg.ReplaceAllString(s, "")
+
+	// сохраняем прямой порядок букв
+	for i := 0; i < len(clean); i++ {
+		straightOrder += string(clean[i])
 	}
-	return true
+
+	// сохраняем реверсивный порядок букв
+	for j := len(clean) - 1; j >= 0; j-- {
+		reverseOrder += string(clean[j])
+	}
+
+	// сравниваем строку с перевернутой
+	return straightOrder == reverseOrder
 }
 
 func main() {
 	var theWord string
-
-	scan, err := fmt.Scan(&theWord)
-	if err != nil {
-		fmt.Println(err)
-	}
-	isPalindrome(theWord)
-
-	fmt.Println(scan)
+	fmt.Scan(&theWord)
+	fmt.Println(isPalindrome(theWord))
 }

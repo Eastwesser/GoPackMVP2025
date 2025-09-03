2092b51ca3a21e6b07bccc8c729efc866a8acad3
Денис Матвеев <alttert@mail.ru>
2025-09-03 04:56:22 +0300

    added unit tests for two sum

--- 6.leet-code/62.task_2_two_sum/main_test.go
+++ 6.leet-code/62.task_2_two_sum/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -18,3 +19,25 @@ func BenchmarkTwoSum(b *testing.B) {
 }
 
 // go test -bench=. -benchmem
+
+func TestTwoSum(t *testing.T) {
+	tests := []struct {
+		name   string
+		nums   []int
+		target int
+		want   []int
+	}{
+		{"Five", []int{1, 2, 3}, 5, []int{1, 2}},
+		{"Ten", []int{2, 6, 5, 9, 4, 3, 7}, 10, []int{1, 4}},
+		{"Hundred", []int{25, 64, 51, 92, 48, 36, 73}, 100, []int{1, 5}},
+	}
+
+	for _, tt := range tests {
+		result := twoSum(tt.nums, tt.target)
+		if !reflect.DeepEqual(result, tt.want) {
+			if len(result) != 2 || result[0] != tt.target || result[1] != tt.target {
+				t.Error("twoSum", tt.nums, tt.target, "expected", tt.want, "got", result)
+			}
+		}
+	}
+}
